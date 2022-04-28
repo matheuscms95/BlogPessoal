efdc9a7417defc00bf55811c6c406283ce79c1d2
matheuscms95 <matheuscms95@gmail.com>
2022-04-28 16:57:53 -0300

    @matheuscms95 [Task: Teste de Contexto] {28/04/2022 16:57}

---
+++ BPVS/BlogPessoal/BlogPessoalTeste/Testes/data/BlogPessoalContextoTeste.cs
@@ -0,0 +1,41 @@
+using BlogPessoal.src.data;
+using BlogPessoal.src.modelos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace BlogPessoalTeste.Testes.data
+{
+    [TestClass]
+    public class BlogPessoalContextoTeste
+    {
+        private BlogPessoalContexto _contexto;
+            
+        [TestInitialize]
+        public void inicio()
+        {
+            var opt = new DbContextOptionsBuilder<BlogPessoalContexto>().
+                UseInMemoryDatabase(databaseName: "db_blogpessoal")
+                .Options;
+
+            _contexto = new BlogPessoalContexto(opt);
+        }
+        
+        [TestMethod]
+        public void InserirNovoUsuarioNoBancoRetornarUsuario()
+        {
+            UsuarioModelo usuario = new UsuarioModelo();
+
+            usuario.Nome = "Matheus Correia";
+            usuario.Email = "matheus@email.com";
+            usuario.Senha = "587469321";
+            usuario.Foto = "AquiVaiOLinkDaFoto";
+
+            _contexto.Usuarios.Add(usuario);
+
+            _contexto.SaveChanges();
+
+            Assert.IsNotNull(_contexto.Usuarios.FirstOrDefault(u => u.Email == "matheus@email.com"));
+        }
+    }
+}
